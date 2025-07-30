be8948ba33b546078ec7f36167a0634d50b45118
gonta1026 <katano.original@gmail.com>
2025-07-30 22:34:16 +0900

    feat: turborepoの雛形を実装

---
+++ packages/ui/components/counter.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import { useState } from "react";
+
+export const Counter: React.FC = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<button id="counter" type="button" onClick={() => setCount(count + 1)}>
+			{count}
+		</button>
+	);
+};
