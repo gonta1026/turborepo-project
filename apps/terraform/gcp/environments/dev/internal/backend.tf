93c2c98448a5c4a1ca926b161fe829741ce4f246
gonta1026 <katano.original@gmail.com>
2025-08-05 06:48:16 +0900

    chore: ファイルを整理する

--- apps/terraform/gcp/environments/dev/internal/backend.tf
+++ apps/terraform/gcp/environments/dev/internal/backend.tf
@@ -5,8 +5,5 @@ terraform {
   backend "gcs" {
     bucket = "terraform-gcp-466623-terraform-state"
     prefix = "dev/internal" # dev環境内のinternal専用プレフィックス
-    # オプション設定
-    # encryption_key = ""  # 顧客管理暗号化キーを使用する場合
-    # impersonate_service_account = "" # サービスアカウントを使用する場合
   }
 }
