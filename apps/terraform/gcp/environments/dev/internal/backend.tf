6bc51ac7322b12f9eea0ad8697d7ec6d7e7a393e
gonta1026 <katano.original@gmail.com>
2025-08-08 00:29:24 +0900

    chore: 一旦使わないであろう、internalを削除

--- apps/terraform/gcp/environments/dev/internal/backend.tf
+++
@@ -1,9 +0,0 @@
-# Remote Backend Configuration for Dev Internal Environment
-# このファイルはTerraformの状態ファイルをGCSに保存するための設定です
-
-terraform {
-  backend "gcs" {
-    bucket = "terraform-gcp-466623-terraform-state"
-    prefix = "dev/internal" # dev環境内のinternal専用プレフィックス
-  }
-}
