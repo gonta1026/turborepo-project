f685ff579c581f8b7b6f1e6ca5cb48919dec4c91
gonta1026 <katano.original@gmail.com>
2025-08-17 12:30:56 +0900

    fix: lint

--- packages/ui/components/counter.tsx
+++ packages/ui/components/counter.tsx
@@ -1,11 +1,12 @@
 import type React from 'react'
-import { useState } from 'react'
+import { useId, useState } from 'react'
 
 export const Counter: React.FC = () => {
   const [count, setCount] = useState(0)
+  const id = useId()
 
   return (
-    <button id="counter" type="button" onClick={() => setCount(count + 1)}>
+    <button id={id} type="button" onClick={() => setCount(count + 1)}>
       {count}
     </button>
   )
